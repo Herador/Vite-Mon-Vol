fcd3db99bf59326d0cfa96fe07fa9a9ef4a61fa7
david watine <david.watine@gmail.com>
2022-03-28 14:52:55 +0200

    premiere sauv

--- Controller/connexion_bdd.php
+++ Controller/connexion_bdd.php
@@ -1,9 +1,7 @@
 <?php
-	// Création de la connexion
-	$connexion = new mysqli("localhost", "root", "", "cuisine");
+	$connexion = new mysqli("localhost", "root", "", "vite_mon_vol");
 	$connexion->set_charset("utf8");
 
-	// Vérification de la connexion
 	if ($connexion->connect_error) {
 		printf("Erreur : Connexion impossible : ". $connexion->connect_error);
 		exit();
