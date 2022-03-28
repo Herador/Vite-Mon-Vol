4ec59027609a153e05ed4bfb264a10fc9ead92c0
maxime marcel <marcel.maxime@free.fr>
2022-03-28 17:01:25 +0200

    card pas fini

--- Controller/liste circuit.php
+++ Controller/liste circuit.php
@@ -1,15 +1,53 @@
-<div class="card w-75">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Button</a>
-  </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="..\Public\CSS\card.css" rel="stylesheet" type="text/css">
+</head>
+<body>
 
-<div class="card w-50">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Button</a>
-  </div>
-</div>
+
+<	<div class="content"><div class="ic"></div>
+			<div class="container_12">
+				<div class="banners">
+					<div class="grid_4">
+						<div class="banner">
+							<img src="..\Public\Image\card_canada1.jpg" alt="">
+							<div class="label">
+								<div class="title">LE CANADA</div>
+								<div class="price"><span></span></div>
+								<a href="#">en savoir plus</a>
+							</div>
+						</div>
+					</div>
+					<div class="grid_4">
+						<div class="banner">
+							<img src="..\Public\Image\card_grece1.jpg" alt="">
+							<div class="label">
+								<div class="title">LA GRECE</div>
+								<div class="price"><span></span></div>
+								<a href="#">en savoir plus</a>
+							</div>
+						</div>
+					</div>
+					<div class="grid_4">
+						<div class="banner">
+							<img src="..\Public\Image\card_tokyo1.jpg" alt="">
+							<div class="label">
+								<div class="title">LE JAPON</div>
+								<div class="price"><span></span></div>
+								<a href="#">en savoir plus</a>
+							</div>
+						</div>
+					</div>
+					
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+</body>
+</html>
