49e554bfd86f82ef59dd30b4ca069dea9b8e594c
maxime marcel <marcel.maxime@free.fr>
2022-03-29 09:29:44 +0200

    base card

--- Controller/liste circuit.php
+++ Controller/liste circuit.php
@@ -1,3 +1,7 @@
+<?php
+require_once ("connexion_bdd.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,49 +9,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="..\Public\CSS\card.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="C:\wamp64\www\Vite-Mon-Vol\Controller\card.css" rel="stylesheet">
 </head>
 <body>
 
 
-<	<div class="content"><div class="ic"></div>
-			<div class="container_12">
-				<div class="banners">
-					<div class="grid_4">
-						<div class="banner">
-							<img src="..\Public\Image\card_canada1.jpg" alt="">
-							<div class="label">
-								<div class="title">LE CANADA</div>
-								<div class="price"><span></span></div>
-								<a href="#">en savoir plus</a>
-							</div>
-						</div>
-					</div>
-					<div class="grid_4">
-						<div class="banner">
-							<img src="..\Public\Image\card_grece1.jpg" alt="">
-							<div class="label">
-								<div class="title">LA GRECE</div>
-								<div class="price"><span></span></div>
-								<a href="#">en savoir plus</a>
-							</div>
-						</div>
-					</div>
-					<div class="grid_4">
-						<div class="banner">
-							<img src="..\Public\Image\card_tokyo1.jpg" alt="">
-							<div class="label">
-								<div class="title">LE JAPON</div>
-								<div class="price"><span></span></div>
-								<a href="#">en savoir plus</a>
-							</div>
-						</div>
-					</div>
-					
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+<div class="card-group">
+  <div class="card">
+    <img src="..\Public\Image\777.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"></h5>
+      <button type="button" class="btn btn-primary">Primary</button>
+      
+    </div>
+  </div>
+  <div class="card">
+    <img src="..\Public\Image\23625.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"></h5>
+      <button type="button" class="btn btn-primary">Primary</button>
+      
+    </div>
+  </div>
+  <div class="card">
+    <img src="..\Public\Image\42004.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"></h5>
+      <button type="button" class="btn btn-primary">Primary</button>
+      
+    </div>
+  </div>
+</div>
 </body>
 </html>
